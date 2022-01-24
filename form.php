dc6e14e6b3096435b1eff1cbf782f12a68c879e2
FlorianCohenJoly <floriancohenjoy@gmail.com>
2022-01-24 16:15:54 +0100

    Create form.php
    creation du formulaire en php

---
+++ form.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>Document</title>
+</head>
+<body> 
+
+
+
+<form method="post" action="form.php">
+
+
+
+<p>
+<p>Veuillez saisir votre nom de compte : </p>
+<input type="text" id="name" name="name" placeholder="Nom"> </input>
+
+<br>
+<p> Veuillez saisir votre identifiant de compte :  </p>
+<input type="text" id="" name="name" placeholder="Votre identifiant de compte"/> 
+
+<br>
+
+<p> Qu'elle est votre type de compte ?  : </p>
+<select id="typeCompte" name="typeCompte">
+        <option value="">--Type de compte--</option>
+        <option value="courant">Compte courant</option>
+        <option value="epargne">Compte epargne</option>
+        <option value="compte joint">Compte joint</option>
+    </select><br>
+
+<br>
+
+<p>Qu'elle est la devise ? </p>
+<select name="Devise">
+            <option value="EUR">Euros</option>
+            <option value="USD">Dollars</option>
+        </select>
+
+<br>
+
+
+<p><input type="submit" name="submitForm" value="Valider"/></p>
+
+</p>
+
+
+    
+</form>
+
+
+
+
+
+    
+</body>
+</html>
